42c9da7e9780bae2d79ee3ce856400c9d1afb0e5
Jose Soto <jsoto7087@gmail.com>
2022-05-02 12:08:23 -0400

    commit tree 156e512ea1552f3248813f9ef9b3c1f6f1930200

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+// document.addEventListener("keydown", e => console.log(e));
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "red"
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left >= 0) {
+        dodger.style.left = `${left - 7}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left < 360) {
+        dodger.style.left = `${left + 7}px`
+    }
+}
+
+
+moveDodgerRight()
+moveDodgerLeft()
